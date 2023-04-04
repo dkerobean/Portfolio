3f1e5795234fbe0a12b743f2fe008410e928cf7e
dickson <dkerobean@gmail.com>
2023-04-04 19:19:19 +0000

    User message submit done

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import UserContact
+from django.contrib import messages
 
 
 def home(request):
@@ -7,6 +9,20 @@ def home(request):
 
 
 def contact(request):
+    
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        message = request.POST["message"]
+            
+        UserContact.objects.create(name=name, email=email, message=message)
+        
+        messages.success(
+            request, 'Your message has been sent. We will get back to you soon.')
+    else:
+        messages.error(
+            request, 'An Error Occured, Try again ')
+    
     
     return render(request, 'main/contact.html')
 
